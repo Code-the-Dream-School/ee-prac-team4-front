ae807410c6a1904831fcb7d4519e04c225faa4a8
alejandro-patino-camargo <alejandropatinoc@gmail.com>
2023-10-22 21:09:16 -0700

    added steps for initial login page setup

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,1 +1,12 @@
-// initial commit 
+//TODO: 
+//1. create a component with the name LoginPage 
+//2. Inside the return() statement, add a div with the className="landingPage"
+//3. Inside the div, add a <h1> tag that displays Login Page
+//4. Inside the div create form with the className="formContainer"
+//5. Add two <input> fields, one fore email, and another for password
+//  * both should be of type text, className="inputField"
+//
+// Nice! you set up the initial login page.
+// Next steps: add a handleSubmit() function for backend connection (error handeling)
+// For now, this ticket is complete!
+// If you think there is something missing, feel free to add more!
