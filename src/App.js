45d52f9a8651d3246b42aa5d892d8f61b5f80fe8
Daria1911 <fetko1992@gmail.com>
2023-10-31 12:15:45 -0400

    fix prettier configuration

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import { getAllData } from './util/index';
 const URL = 'http://localhost:8000/api/v1/';
 
 function App() {
-
-const [message, setMessage] = useState(''); 
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
-
     (async () => {
-      const myData = await getAllData(URL)
+      const myData = await getAllData(URL);
       setMessage(myData.data);
     })();
-      
+
     return () => {
       console.log('unmounting');
-    }
-
+    };
   }, []);
 
   return (
