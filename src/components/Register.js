91d082e350cbe33cfa007ab89d84927576893afe
Alejandro Patino Camargo <alejandropatinoc@gmail.com>
2023-11-01 11:15:29 -0700

    connecting to backend routes to store new credentials - not tested yet

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,20 +1,54 @@
 import React, { useState } from 'react';
+
 function Register() {
   const [newEmail, setNewEmail] = useState('');
   const [newPassword, setNewPassword] = useState('');
+  const [error, setError] = useState(null);
 
-  const handleSubmit = (e) => {
+  async function signUpForm(e) {
     e.preventDefault();
-    // add logic connectin gto backend repo for user auth
-  };
+    try {
+      const response = await fetch(`https://localhost:8000/api/v1/auth/register`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: newEmail,
+          password: newPassword,
+          email: newEmail
+        })
+      });
+
+      if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(`Network response was not ok: ${errorMessage}`);
+      }
+
+      const data = await response.json();
+      window.alert(`Welcome, ${data.username}`);
+      setError(null);
+      /*TODO:
+       * 1. logic for successful/failed attempts
+       * 2. data.hasUsername - prompt "username aready exists"
+       * 3. data.hasEmail - prompts  "email already exists"
+       */
+
+      return data;
+    } catch (error) {
+      console.error('Error during registration:', error);
+      setError('Error during registration. Please try again.');
+      throw error;
+    }
+  }
 
   return (
     <div className='registerPage'>
-      <h1> Register</h1>
+      <h1>Register</h1>
 
-      <form onSubmit={handleSubmit} className='registerContainer'>
+      <form onSubmit={(e) => signUpForm(e)} className='registerContainer'>
         <input
-          type='text'
+          type='email'
           placeholder='janedoe@gmail.com'
           value={newEmail}
           onChange={(e) => setNewEmail(e.target.value)}
@@ -23,7 +57,7 @@ function Register() {
         />
 
         <input
-          type='text'
+          type='password'
           placeholder='enter a password'
           value={newPassword}
           onChange={(e) => setNewPassword(e.target.value)}
@@ -34,6 +68,9 @@ function Register() {
         <button type='submit' className='submitButton'>
           submit
         </button>
+
+        {/*initial prompting for error message, will change  with css*/}
+        {error && <div style={{ color: 'red' }}>{error}</div>}
       </form>
     </div>
   );
