5985fb100a64d78ed80a556924e63ddee8ba30c6
Alzhan-B <alzhan.braliyev@gmail.com>
2023-11-10 17:51:53 -0500

    PRACT_43_create_flashcard_component

---
+++ src/components/flashcard/Flashcard.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./Flashcard.css";
+
+const FlashCard = ({ question, answer }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [showHint, setShowHint] = useState(false);
+
+  var mockData = {
+    question: "What is our project group number?",
+    answer: "4",
+    hint: "The group number is a single-digit number",
+    tags: ["code the dream", "practicum"],
+  };
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  const handleHint = () => {
+    setShowHint(!showHint);
+  };
+
+  return (
+    <div className="card">
+      {/* <div className={`card-inner ${isFlipped ? "flipped" : ""}`}> */}
+      {!isFlipped ? (
+        <div className="card-front">
+          <h3>
+            <button className="hint-button" onClick={handleHint}>
+              {showHint ? "Hide Hint" : "Show Hint"}
+            </button>
+          </h3>
+          {showHint && <p className="hint-text">{mockData.hint}</p>}
+          <h3> Question: </h3>
+          <p>{mockData.question}</p>
+          <button onClick={handleFlip}>Show Answer</button>
+        </div>
+      ) : (
+        <div className="card-back">
+          <h3> Correct answer: </h3>
+          <p>{mockData.answer}</p>
+          <button onClick={() => console.log("NEXT")}>Keep in my deck</button>
+          <button onClick={() => console.log("Learned")}>Yeah! I got it</button>
+        </div>
+      )}
+      {/* </div> */}
+    </div>
+  );
+};
+
+export default FlashCard;
