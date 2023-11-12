5c0dddf84097f86746ca55cb752d1d3c00f5b56e
elenademi <118300498+elenademi@users.noreply.github.com>
2023-11-11 20:37:33 -0500

    remove placeholders from the email and password fields

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -8,21 +8,21 @@ function LoginPage() {
     <div className="loginPage">
       <h1>Login Page</h1>
       <form className="formContainer">
-        <label htmlFor="email"></label>
+        <label htmlFor="email">Email</label>
         <input
           className="inputField"
           type="email"
           name="email"
           id="email"
-          placeholder="Enter your email"
+          
         ></input>
-        <label htmlFor="password"></label>
+        <label htmlFor="password">Password</label>
         <input
           className="inputField"
           type="password"
           name="password"
           id="password"
-          placeholder="Enter your password"
+          
         ></input>
       </form>
       <button type="submit" onClick={handleSubmit}>
