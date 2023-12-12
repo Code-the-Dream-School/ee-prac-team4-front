79cf33f9686c21a6553b61f0b6195a5cd5e7e9b7
koral14 <105457134+koral14@users.noreply.github.com>
2023-12-11 19:35:04 -0800

    updated styling for the aside navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../App";
 import "./Navbar.css";
@@ -10,10 +10,32 @@ function Navbar({ openRigthNav, setIsOpenRightNav }) {
   const { isLoggedIn, userData, handleLogout } = useContext(AuthContext);
   const [isHovered, setIsHovered] = useState(false);
   const [checkbox, checkCheckbox] = useState(false);
+  const sidebarRef = useRef(null);
 
-  // const handleSidebarToggle = (isOpen) => {
-  //   checkCheckbox(isOpen);
-  // };
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        checkCheckbox(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  const handleCheckboxClick = (event) => {
+    event.stopPropagation(); 
+
+    if (checkbox) {
+      checkCheckbox(!checkbox);
+    } else {
+      checkCheckbox(!checkbox);
+    }
+    
+  };
 
   return (
     <div className="nav">
@@ -30,51 +52,57 @@ function Navbar({ openRigthNav, setIsOpenRightNav }) {
         </Link>
       </div>
 
-      <label className="hamburger-menu">
+      <label className="hamburger-menu" >
         <input 
           type='checkbox' 
           id='hamburger-checkbox' 
           name='hamburger-checkbox' 
           checked={checkbox} 
-          onChange={() => {checkCheckbox(!checkbox)}}
+          onChange={handleCheckboxClick}
           className={`hamburger ${openRigthNav ? 'menu' : 'hamburger'}`}
         />
       </label>
-      <aside className={`sidebar ${checkbox ? 'open' : ''}`}>
+      <aside 
+        className={`sidebar ${checkbox ? 'open' : ''}`}
+        ref={sidebarRef}
+      >
         <ul className="right-nav-ul">
           <div className="buttons">
             <div className="first-group">
+            <Link to="/resources" className="navbar-button" onClick={() => console.log('Button Resources clicked')}>
               <li>
-                <Link to="/resources" className="navbar-button">Resources</Link>
+                Resources
               </li>
+            </Link>
+            <Link to="/" className="navbar-button" onClick={() => console.log('Button About clicked')}>
               <li>
-                <Link to="/" className="navbar-button">About</Link>
+                About
               </li>
+            </Link>
             </div>
             
             <div className="second-group">
               {!isLoggedIn ? (
                 <div className="authButtons">
-                  <Link to="/login">
+                  <Link to="/login" className="link-auth" onClick={() => console.log('Button 1 clicked')}>
                     <Button className="log-in-button" type="submit" buttonText="Log-in" />
                   </Link>
-                  <Link to="/login">
-                    <Button className="sign-up-button" type="submit" buttonText="Sign-up" />
+                  <Link to="/register" className="link-auth">
+                    <Button className="sign-up-button" type="submit" buttonText="Sign-up" onClick={() => console.log('Button 2 clicked')}/>
                   </Link>
                 </div>
               ) : (
                 <div className="loginItems">
                   {/* update when registration route returns full user data */}
                   <p className="username">{userData.user?.username}</p>
-                  <Link to="/login" onClick={handleLogout} className="login-button">
-                    <button className='log-out-button' title='Logout'>
+                  <Link to="/login" onClick={() => { handleLogout() }} className="login-button">
+                    <button className='log-out-button' title='Logout' onClick={() => console.log('Button 3 clicked')}>
                       <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"/></svg>
                     </button>
                   </Link>
                 </div>
               )}
             </div>
-            
           </div>
         </ul>
       </aside>
